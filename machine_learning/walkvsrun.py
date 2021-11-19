86c0b1894e1c12055e8fdfbee249ca6d4145b2ab
suku1612 <44553614+sukumar1612@users.noreply.github.com>
2021-11-19 10:57:37 +0530

    test2

--- machine_learning/walkvsrun.py
+++ machine_learning/walkvsrun.py
@@ -1,18 +1,22 @@
-import pandas as pd
-import numpy
 import joblib
+import pandas as pd
+
+
 def predict_data(data):
-    #predict_data([[-0.700,0.300,1.000],[-1.600,0.400,1.900],[-7.300,-11.400,0.700]])
-    print(type(data[0][0]),"___________sdkjfsd______________")
-    df=pd.DataFrame(data)
-    df['total_acc']=df[0]**2+df[1]**2+df[2]**2
-    cls=joblib.load('model_finale.sav')
-    d=df.to_numpy()
-    x=cls.predict(d)
-    x=list(x)
-    p=(x.count(1)/len(x))
-    time_running=((20*len(x))/1000)*p
-    p1=(x.count(0)/len(x))
-    time_walking=((20*len(x))/1000)*p1
-    print([time_running+time_walking,time_walking,time_running])
-    return [time_running+time_walking,time_walking,time_running]
+    # predict_data([[-0.700,0.300,1.000],[-1.600,0.400,1.900],[-7.300,-11.400,0.700]])
+    print(type(data[0][0]), "___________sdkjfsd______________")
+    data = [list(map(int, i)) for i in data]
+    print(type(data[0][0]), "___________new_sdkjfsd______________")
+
+    df = pd.DataFrame(data)
+    df['total_acc'] = df[0] ** 2 + df[1] ** 2 + df[2] ** 2
+    cls = joblib.load('model_finale.sav')
+    d = df.to_numpy()
+    x = cls.predict(d)
+    x = list(x)
+    p = (x.count(1) / len(x))
+    time_running = ((20 * len(x)) / 1000) * p
+    p1 = (x.count(0) / len(x))
+    time_walking = ((20 * len(x)) / 1000) * p1
+    print([time_running + time_walking, time_walking, time_running])
+    return [time_running + time_walking, time_walking, time_running]
